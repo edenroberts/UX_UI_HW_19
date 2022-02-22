40fc61dbf04954d069088bfba0ee50c79732b035
edenroberts <90351082+edenroberts@users.noreply.github.com>
2022-02-22 15:31:05 -0500

    javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 console.log("Your index.js file is loaded correctly!");
+
+var helloWorld = "HelloWorld!";
+console.log(helloWorld)
+
+$(document).ready(function(){
+    $(“li”).hover(function(){
+        $(this).css(“font-size”, “30px”);
+        }, function(){
+        $(this).css(“font-size”, “20px”);
+    });
